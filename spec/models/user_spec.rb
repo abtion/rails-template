a03a7eb49d6bfc07ade772f2dc47f6ed39932292
Thor-Bjørn Böhme <tb@abtion.com>
2019-12-18 09:29:03 +0100

    Add specs for password-length validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,23 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  describe "password" do
+    context "password 8 characters long" do
+      it "user valid" do
+        user = build(:user, password: "12345678")
+        expect(user).to be_valid
+      end
+    end
+
+    context "password less than 8 characters long" do
+      it "user not valid" do
+        user = build(:user, password: "1234")
+        expect(user).to_not be_valid
+        expect(user.errors[:password]).to eq(["is too short (minimum is 8 characters)"])
+      end
+    end
+  end
+
   describe "admin role" do
     let(:user) { create(:user) }
 
