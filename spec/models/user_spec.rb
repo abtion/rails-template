a46be5303b3f422f8885ad3c0cee8d77beb25118
Hari <haricarreras@gmail.com>
2019-12-19 09:11:07 +0100

    Update minimum password length to 8 (#18)
    * Update minimum password length to 8
    
    New Abtion security policy.
    
    * Explicitly use min-password-length of 8 for Faker
    
    This improves clarity while also being ever so slightly more robust.
    
    * Add specs for password-length validation
    
    * Improve context block wording
    
    `context` descriptions should start with `with/out` or `when` to make it more readable.
    `it` descriptions should contain a verb.
    More info here: http://www.betterspecs.org/#contexts
    
    * Adjust failing test case to actually fail

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,23 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  describe "password" do
+    context "with 8 character long password" do
+      it "user is valid" do
+        user = build(:user, password: "12345678")
+        expect(user).to be_valid
+      end
+    end
+
+    context "without 8 character long password" do
+      it "user is not valid" do
+        user = build(:user, password: "1234567")
+        expect(user).to_not be_valid
+        expect(user.errors[:password]).to eq(["is too short (minimum is 8 characters)"])
+      end
+    end
+  end
+
   describe "admin role" do
     let(:user) { create(:user) }
 
