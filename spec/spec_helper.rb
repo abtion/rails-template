0688c65385253690014c80fd0c624a3be24fb077
Pierre-Arnaud Baciocchini <pierre.baciocchini@gmail.com>
2020-01-08 16:17:33 +0100

    Extract Capybara and FactoryBot configurations to their own files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,41 +8,6 @@ Dotenv.load(
   ".env"
 )
 
-require "factory_bot"
-require "capybara/rspec"
-require "webdrivers/chromedriver"
-
-chrome_bin = ENV.fetch("GOOGLE_CHROME_SHIM", nil)
-
-Selenium::WebDriver::Chrome.path = chrome_bin if chrome_bin
-chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
-
-Capybara.configure do |config|
-  config.server_port = 9887 + ENV["TEST_ENV_NUMBER"].to_i
-end
-
-Capybara.default_max_wait_time = 5
-
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
-  )
-end
-
-Capybara.register_driver :headless_chrome do |app|
-  Capybara::Selenium::Driver.load_selenium
-  browser_options = ::Selenium::WebDriver::Chrome::Options.new
-  browser_options.args << "--headless"
-  browser_options.args << "--disable-gpu"
-  browser_options.args << "--window-size=1920,1080"
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
-end
-
-Capybara.javascript_driver = ENV.fetch("CAPYBARA_DRIVER", "headless_chrome").to_sym
-Capybara.default_driver = ENV.fetch("CAPYBARA_DRIVER", "headless_chrome").to_sym
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -51,9 +16,6 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
-  # Include FactoryBot's methods in the suite for more concise usage
-  config.include FactoryBot::Syntax::Methods
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.order = :random
---
+++ spec/support/capybara.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "capybara/rspec"
+require "webdrivers/chromedriver"
+
+chrome_bin = ENV.fetch("GOOGLE_CHROME_SHIM", nil)
+
+Selenium::WebDriver::Chrome.path = chrome_bin if chrome_bin
+chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
+Capybara.configure do |config|
+  config.server_port = 9887 + ENV["TEST_ENV_NUMBER"].to_i
+end
+
+Capybara.default_max_wait_time = 5
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+  )
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  Capybara::Selenium::Driver.load_selenium
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  browser_options.args << "--headless"
+  browser_options.args << "--disable-gpu"
+  browser_options.args << "--window-size=1920,1080"
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+
+Capybara.javascript_driver = ENV.fetch("CAPYBARA_DRIVER", "headless_chrome").to_sym
+Capybara.default_driver = ENV.fetch("CAPYBARA_DRIVER", "headless_chrome").to_sym
