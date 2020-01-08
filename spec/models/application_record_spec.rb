0ef53fddbcfdbb78b9571d468886e2dd83f30df1
martinvintherp <martinvintherp@gmail.com>
2020-01-08 21:04:36 +0100

    Configured so ordering are using created_at
    When using uuid's the active record methods 'first' and 'last' doesn't
    work as expected, given that active record expects an auto incremented
    id

---
+++ spec/models/application_record_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ApplicationRecord, type: :model do
+  describe "uuid ordering" do
+    it "implicitly orders by created_at" do
+      expect(ApplicationRecord.implicit_order_column).to eq("created_at")
+    end
+  end
+end
