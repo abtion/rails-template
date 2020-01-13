3cf0f61df70df2de1e3baa0a2bb5413eef1e64ff
Pierre-Arnaud Baciocchini <pierre.baciocchini@gmail.com>
2020-01-13 15:07:16 +0100

    test

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,18 +3,15 @@
 require "capybara/rspec"
 require "webdrivers/chromedriver"
 
-chrome_bin = ENV.fetch("GOOGLE_CHROME_SHIM", nil)
-
-Selenium::WebDriver::Chrome.path = chrome_bin if chrome_bin
-chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
-
-Capybara.configure do |config|
-  config.server_port = 9887 + ENV["TEST_ENV_NUMBER"].to_i
-end
-
-Capybara.default_max_wait_time = 5
-
 Capybara.register_driver :chrome do |app|
+  chrome_bin = ENV.fetch("GOOGLE_CHROME_SHIM", nil)
+  chrome_opts = if chrome_bin
+    Selenium::WebDriver::Chrome.path = chrome_bin
+    { "chromeOptions" => { "binary" => chrome_bin } }
+  else
+    {}
+  end
+
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
@@ -31,5 +28,17 @@ Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 end
 
+Capybara.register_driver :headless_firefox do |app|
+  Capybara::Selenium::Driver.load_selenium
+  browser_options = ::Selenium::WebDriver::Firefox::Options.new
+  browser_options.args << "--headless"
+  Capybara::Selenium::Driver.new(app, browser: :firefox, options: browser_options)
+end
+
 Capybara.javascript_driver = ENV.fetch("CAPYBARA_DRIVER", "headless_chrome").to_sym
 Capybara.default_driver = ENV.fetch("CAPYBARA_DRIVER", "headless_chrome").to_sym
+
+Capybara.default_max_wait_time = 5
+Capybara.configure do |config|
+  config.server_port = 9887 + ENV["TEST_ENV_NUMBER"].to_i
+end
