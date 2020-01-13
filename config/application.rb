72bf7a0bb384b33194058ab06c4f9557d963b13f
Pierre-Arnaud Baciocchini <pierre.baciocchini@icloud.com>
2020-01-13 12:36:19 +0100

    Disable rubocop check for application name case (#32)

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,9 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# rubocop:disable Naming/ClassAndModuleCamelCase
 module PROJECT_NAME_PASCAL
+  # rubocop:enable Naming/ClassAndModuleCamelCase
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
