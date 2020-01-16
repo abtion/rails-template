d3417fadc171f6fbc0f011b696661c9781242f50
Pierre-Arnaud Baciocchini <pierre.baciocchini@icloud.com>
2020-01-16 07:35:07 +0100

    Prevent timing attacks in API authentication (#36)
    * Prevent timing attacks in API authentication
    
    By first finding the user by its ID, and then comparing the
    authentication tokens, we prevent timing attacks.
    This solution however requires sending the user's ID alongside its
    token.

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -8,15 +8,20 @@ module Api
       private
 
       def require_authentication!
-        user = authenticate_or_request_with_http_token do |token, _options|
-          User.find_by(authentication_token: token)
-        end
+        user = User.find(request.headers["X-User-Id"])
 
-        if user.present?
+        if user.present? && valid_token?(user.authentication_token)
           sign_in user
         else
           render status: :unauthorized
         end
+      end
+
+      def valid_token?(token)
+        ActiveSupport::SecurityUtils.secure_compare(
+          "Token #{token}",
+          request.headers["Authorization"]
+        )
       end
     end
   end
