d4cc6879bc45430b51c12500c36e2baedb7553ce
Thor-Bjørn Böhme <tb@abtion.com>
2020-04-29 17:23:11 +0200

    Add specs for Ability

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require "cancan/matchers"
+
+RSpec.describe Ability, type: :model do
+  context "when user is admin" do
+    it "is able to manage all" do
+      admin = build(:user, :admin)
+      ability = Ability.new(admin)
+
+      expect(ability).to be_able_to(:manage, :all)
+    end
+  end
+end
