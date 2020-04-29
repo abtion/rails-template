c19565a59a5c79ffb84871c1762f74dd7e9d25e1
Thor-Bjørn Böhme <tb@abtion.com>
2020-04-29 21:08:14 +0200

    Feature: Add specs for Ability (#56)
    * Add specs for Ability
    
    * Add Ability specs for normal User
    
    * Add Ability specs for nil user

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require "cancan/matchers"
+
+RSpec.describe Ability, type: :model do
+  context "when user is admin" do
+    it "is able to manage all" do
+      admin = build(:user, :admin)
+      ability = Ability.new(admin)
+
+      expect(ability).to be_able_to(:manage, :all)
+    end
+  end
+
+  context "when user is user" do
+    it "is not able to manage all" do
+      admin = build(:user)
+      ability = Ability.new(admin)
+
+      expect(ability).to_not be_able_to(:manage, :all)
+    end
+  end
+
+  context "when no user given" do
+    it "doesn't explode and isn't able to manage all" do
+      ability = Ability.new(nil)
+
+      expect(ability).to_not be_able_to(:manage, :all)
+    end
+  end
+end
