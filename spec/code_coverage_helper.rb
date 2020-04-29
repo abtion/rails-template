46cb7a437bc6c7b1353edb168ea0c5ddb7e6e458
Thor-Bjørn Böhme <tb@abtion.com>
2020-04-29 11:45:05 +0200

    Set minimum code coverage to 73%
    73% was choose because that is the closest pretty number to our current
    coverage ratio.

--- spec/code_coverage_helper.rb
+++ spec/code_coverage_helper.rb
@@ -6,5 +6,6 @@ unless ENV["DISABLE_SIMPLECOV"] == "true"
   SimpleCov.start("rails") do
     add_filter "spec"
     add_filter "vendor"
+    minimum_coverage((ENV["CODE_COVERAGE_PERCENTAGE"] || 0).to_i)
   end
 end
