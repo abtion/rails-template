c2f3b1f4b7a04003aad617ee6235fe9a3b836265
Thor-Bjørn Böhme <tb@abtion.com>
2020-04-29 11:32:03 +0200

    Add a NullUser just for Geoff

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -8,12 +8,18 @@ module Api
       private
 
       def require_authentication!
-        user = User.find_by(id: request.headers["X-User-Id"])
+        user = User.find_by(id: request.headers["X-User-Id"]) || NullUser.new
 
-        if user&.valid_token?(request.headers["Authorization"])
+        if user.valid_token?(request.headers["Authorization"])
           sign_in user
         else
           head :unauthorized
+        end
+      end
+
+      class NullUser
+        def valid_token?(_token)
+          false
         end
       end
     end
