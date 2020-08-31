c9c5c9855398323fe7400628b09394f5f13e60b5
Thor-Bjørn Böhme <tb@abtion.com>
2020-08-31 11:12:19 +0200

    Add spec for devise translations
    Co-authored-by: Jesper Sørensen <jeppester1@gmail.com>
    Co-authored-by: Pierre-Arnaud Baciocchini <pb@abtion.com>

---
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "devise/sessions/new", type: :view do
+  def stub_devise_helpers
+    without_partial_double_verification do
+      allow(view).to receive(:resource) { User.new }
+      allow(view).to receive(:resource_name) { :user }
+      allow(view).to receive(:devise_mapping) { Devise.mappings[:user] }
+    end
+  end
+
+  context "with danish locale" do
+    before { I18n.locale = :da }
+    after { I18n.locale = I18n.default_locale }
+
+    it "displays danish text" do
+      stub_devise_helpers
+
+      render
+
+      expect(rendered).to have_selector("h2", exact_text: "Log ind")
+    end
+  end
+
+  context "with english locale" do
+    before { I18n.locale = :en }
+    after { I18n.locale = I18n.default_locale }
+
+    it "displays english text" do
+      stub_devise_helpers
+
+      render
+
+      expect(rendered).to have_selector("h2", exact_text: "Log in")
+    end
+  end
+end
