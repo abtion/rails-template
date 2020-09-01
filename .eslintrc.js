526d66f3f1a056bcd3926e19a469333a792c1126
Thor-Bjørn Böhme <tb@abtion.com>
2020-09-01 19:31:02 +0200

    Feature: Setup eslint (#92)
    * Add ESLint to project
    
    Co-authored-by: Jesper Sørensen <jeppester1@gmail.com>
    
    * Add ESLint to github actions
    
    Co-authored-by: Jesper Sørensen <jeppester1@gmail.com>
    
    * Run ESLint
    
    Co-authored-by: Jesper Sørensen <jeppester1@gmail.com>
    Co-authored-by: Markus von Obelitz Munk Søe <mo@abtion.com>
    
    Co-authored-by: Jesper Sørensen <jeppester1@gmail.com>
    Co-authored-by: Markus von Obelitz Munk Søe <mo@abtion.com>

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "env": {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    sourceType: "module",
+  },
+  "rules": {
+    // enable additional rules
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "double"],
+    "quote-props": ["error", "consistent"],
+
+    // Don't allow console.log
+    "no-console": ["error"]
+  },
+};
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -6,13 +6,15 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require('bootstrap')
+require("bootstrap")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+require.context("../images", true)
 
 require("../stylesheets/application.scss")
+
