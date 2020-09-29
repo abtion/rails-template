3623c8865bcae02f5eeccaff1bd4f862f4c16a89
Thor-Bjørn Böhme <tb@abtion.com>
2020-09-29 16:16:16 +0200

    Move conditional to `before_action`
    This way we don't check `ENV` vars in every request.
    
    Co-authored-by: Mathias Jean Johansen <mj@abtion.com>
    Co-authored-by: Jesper Sørensen <jeppester1@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied, with: :access_denied
-  before_action :basic_auth_wall
+  before_action :basic_auth_wall, if: -> { ENV["HTTP_AUTH_PASSWORD"].present? }
 
   protected
 
@@ -13,8 +13,6 @@ class ApplicationController < ActionController::Base
   private
 
   def basic_auth_wall
-    return unless ENV["HTTP_AUTH_PASSWORD"]
-
     authenticate_or_request_with_http_basic do |username, password|
       ActiveSupport::SecurityUtils.secure_compare(username, ENV["HTTP_AUTH_USERNAME"]) &
         ActiveSupport::SecurityUtils.secure_compare(password, ENV["HTTP_AUTH_PASSWORD"])
