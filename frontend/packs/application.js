89ae9c4bc02d42afd15ec350c857da90b14fb0e4
Mathias Jean Johansen <mj@abtion.com>
2020-09-29 10:10:49 +0200

    Revert "Remove ActionCable."
    This reverts commit 026e2be73f338b890a2b3a5125b0aac8477052ad.

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -5,6 +5,7 @@
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
+require("channels")
 require("bootstrap")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
