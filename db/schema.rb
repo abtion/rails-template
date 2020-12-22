8a7bdea0ed97093781665239975e0aab4f4fbe79
Pierre-Arnaud Baciocchini <pb@abtion.com>
2020-12-22 15:24:53 +0100

    Exclude db/schema.rb from rubocop
    Since the file is auto-generated, it does not make sense to
    apply rubocop rules on it afterwards. This will also prevent
    having a diff in the file after running all migrations.
    
    Co-authored-by: Martin Petersen <mp@abtion.com>

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,24 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_522_002_904) do
+ActiveRecord::Schema.define(version: 2019_05_22_002904) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string("name")
-    t.string("email", default: "", null: false)
-    t.string("encrypted_password", default: "", null: false)
-    t.boolean("admin", default: false, null: false)
-    t.string("authentication_token", null: false)
-    t.string("reset_password_token")
-    t.datetime("reset_password_sent_at")
-    t.datetime("remember_created_at")
-    t.datetime("created_at", precision: 6, null: false)
-    t.datetime("updated_at", precision: 6, null: false)
-    t.index(["authentication_token"], name: "index_users_on_authentication_token")
-    t.index(["email"], name: "index_users_on_email", unique: true)
-    t.index(["reset_password_token"], name: "index_users_on_reset_password_token", unique: true)
+    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.boolean "admin", default: false, null: false
+    t.string "authentication_token", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["authentication_token"], name: "index_users_on_authentication_token"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
