410ed8c2a4863d12781029e7dfd8d875e8504da4
Pierre-Arnaud Baciocchini <pb@abtion.com>
2020-12-23 09:41:12 +0100

    Use default 'selenium_chrome_headless' driver instead of our own
    The options were very similar, expect for the window size, which
    is not relevant.
    
    Co-authored-by: Martin Petersen <mp@abtion.com>

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,19 +3,13 @@
 require "capybara/rspec"
 require "webdrivers/chromedriver" unless ENV.key?("DISABLE_WEBDRIVERS")
 
-Capybara.register_driver(:headless_chrome) do |app|
-  Capybara::Selenium::Driver.load_selenium
-  browser_options = ::Selenium::WebDriver::Chrome::Options.new
-  browser_options.args << "--headless"
-  browser_options.args << "--disable-gpu"
-  browser_options.args << "--window-size=1920,1080"
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
-end
+# Default driver registrations
+# https://github.com/teamcapybara/capybara/blob/master/lib/capybara/registrations/drivers.rb
 
 Capybara.configure do |config|
   config.server_port = 9887 + ENV["TEST_ENV_NUMBER"].to_i
   config.default_max_wait_time = 5
   config.server = :puma, { Silent: true }
-  config.default_driver = ENV.fetch("CAPYBARA_DRIVER", "headless_chrome").to_sym
-  config.javascript_driver = ENV.fetch("CAPYBARA_DRIVER", "headless_chrome").to_sym
+  config.default_driver = ENV.fetch("CAPYBARA_DRIVER", "selenium_chrome_headless").to_sym
+  config.javascript_driver = ENV.fetch("CAPYBARA_DRIVER", "selenium_chrome_headless").to_sym
 end
