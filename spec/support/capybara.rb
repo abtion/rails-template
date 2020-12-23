1dd08422c754b7c7a4339f2a9817b3f679d3daea
Pierre-Arnaud Baciocchini <pb@abtion.com>
2020-12-23 09:44:43 +0100

    Simplify Capybara setup (#110)
    * Simplify Capybara setup
    * Move the javascript related support helper to its own file.
    * Use default 'selenium_chrome_headless' driver instead of our own
    
    The options were very similar, expect for the window size, which
    is not relevant.
    
    Co-authored-by: Martin Petersen <mp@abtion.com>

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,50 +3,13 @@
 require "capybara/rspec"
 require "webdrivers/chromedriver" unless ENV.key?("DISABLE_WEBDRIVERS")
 
-chrome_bin = ENV.fetch("GOOGLE_CHROME_SHIM", nil)
-
-Selenium::WebDriver::Chrome.path = chrome_bin if chrome_bin
-chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+# Default driver registrations
+# https://github.com/teamcapybara/capybara/blob/master/lib/capybara/registrations/drivers.rb
 
 Capybara.configure do |config|
   config.server_port = 9887 + ENV["TEST_ENV_NUMBER"].to_i
-end
-
-Capybara.default_max_wait_time = 5
-Capybara.server = :puma, { Silent: true }
-
-Capybara.register_driver(:chrome) do |app|
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
-  )
-end
-
-Capybara.register_driver(:headless_chrome) do |app|
-  Capybara::Selenium::Driver.load_selenium
-  browser_options = ::Selenium::WebDriver::Chrome::Options.new
-  browser_options.args << "--headless"
-  browser_options.args << "--disable-gpu"
-  browser_options.args << "--window-size=1920,1080"
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
-end
-
-Capybara.javascript_driver = ENV.fetch("CAPYBARA_DRIVER", "headless_chrome").to_sym
-Capybara.default_driver = ENV.fetch("CAPYBARA_DRIVER", "headless_chrome").to_sym
-
-# This will fail feature specs on javascript errors/warnings
-RSpec.configure do |config|
-  config.after(:each, type: :feature) do
-    errors = page.driver.browser.manage.logs.get(:browser)
-
-    if errors.present?
-      aggregate_failures "javascript errors/warnings" do
-        errors.each do |error|
-          expect(error.level).not_to(eq("SEVERE"), error.message)
-          expect(error.level).not_to(eq("WARNING"), error.message)
-        end
-      end
-    end
-  end
+  config.default_max_wait_time = 5
+  config.server = :puma, { Silent: true }
+  config.default_driver = ENV.fetch("CAPYBARA_DRIVER", "selenium_chrome_headless").to_sym
+  config.javascript_driver = ENV.fetch("CAPYBARA_DRIVER", "selenium_chrome_headless").to_sym
 end
