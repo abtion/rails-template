e977e632d8b2e12e1bcfdb4bdd0b6f7b3724991e
Rune Schjellerup Philosof <rp@abtion.com>
2021-02-22 15:55:21 +0100

    Run rails app:update
    Co-authored-by: Thor-Bjørn Böhme <tb@abtion.com>

--- config/application.rb
+++ config/application.rb
@@ -14,51 +14,24 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "rails/test_unit/railtie"
+# require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 module ProjectNamePascal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults(6.0)
 
-    # Permitted locales available for the application
-    I18n.available_locales = [:en, :da]
-
-    # Set default locale to something other than :en
-    I18n.default_locale = :en
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Record the DB schema in SQL format for accuracy
-    config.active_record.schema_format = :ruby
-
-    config.active_job.queue_adapter = :sidekiq
-
-    config.generators do |g|
-      # Use UUID primary keys by default
-      g.orm(:active_record, primary_key_type: :uuid)
-
-      # Set RSpec as the test framework, with FactoryBot covering fixtures/factories
-      g.test_framework(:rspec)
-      g.fixture_replacement(:factory_bot, dir: "spec/factories")
-
-      # Don't generate system test files.
-      g.system_tests = nil
-    end
-
-    # Configure Pry as the default rails console
-    console do
-      # this block is called only when running console,
-      # so we can safely require pry here
-      require "pry"
-      config.console = Pry
-    end
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
