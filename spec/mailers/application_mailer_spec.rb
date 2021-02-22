31ea6a07e45d07eaa89309fb0c923e74b3ac3a3d
Thor-Bjørn Böhme <tb@abtion.com>
2021-02-22 18:55:09 +0100

    Add test for default from in ApplicationMailer
    This gets rid of 50% of pending specs.

--- spec/mailers/application_mailer_spec.rb
+++ spec/mailers/application_mailer_spec.rb
@@ -3,5 +3,22 @@
 require "rails_helper"
 
 RSpec.describe ApplicationMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    test_mailer = Class.new(ApplicationMailer) do
+      def test_mail
+        mail(to: "test@example.com", subject: "test") do |format|
+          format.text do
+            render plain: "Test Text"
+          end
+        end
+      end
+    end
+    stub_const("TestMailer", test_mailer)
+  end
+
+  it "has a default from" do
+    mail = TestMailer.test_mail
+
+    expect(mail.from).to match_array("from@example.com")
+  end
 end
