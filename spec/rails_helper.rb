592d331b7fba9b0e03ea1f8d3de1b82f1c0c9bb2
Jesper Sørensen <jeppester1@gmail.com>
2021-06-22 08:32:16 +0200

    Update ruby (#348)
    * Update to ruby 3.0.1
    
    * Remove redundant sort

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
-Dir.glob("./spec/support/**/*.rb").sort.each { |f| require f }
+Dir.glob("./spec/support/**/*.rb").each { |f| require f }
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
