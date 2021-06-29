ba78de054a181886c154a9fbba1094b66ac8096f
Viesturs Beinarovics <vb@abtion.com>
2021-06-29 14:40:25 +0200

    Update bootstrap v4 -> v5

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,8 +5,7 @@ environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default'],
+    jQuery: 'jquery'
   })
 )
 
