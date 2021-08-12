d5c81d0f5a9513c2a8d708769fb946d00aad9b32
Jesper Sørensen <jeppester1@gmail.com>
2021-08-12 13:48:40 +0200

    Update config/initializers/bullet.rb
    Co-authored-by: runephilosof-abtion <57357936+runephilosof-abtion@users.noreply.github.com>

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
 
     if Rails.env.test?
       Bullet.raise = true # raise an error if n+1 query occurs
+      Bullet.unused_eager_loading_enable = false
     end
   end
 end
