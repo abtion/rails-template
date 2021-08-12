35a87fd5e9912cfd22e4e8e53b54219b1d9307ba
runephilosof-abtion <57357936+runephilosof-abtion@users.noreply.github.com>
2021-08-12 10:16:28 +0200

    Lint: indentation
    They were not aligned properly.
    I prefer to not align them at all, because if you then add a longer entry, you change irrelevant lines, polluting the diff.

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -4,18 +4,18 @@ return if Rails.env.production?
 
 Rails.application.configure do
   config.after_initialize do
-    Bullet.enable        = true
+    Bullet.enable = true
     Bullet.bullet_logger = true
 
     if Rails.env.development?
-      Bullet.alert         = true
-      Bullet.console       = true
-      Bullet.rails_logger  = true
-      Bullet.add_footer    = true
+      Bullet.alert = true
+      Bullet.console = true
+      Bullet.rails_logger = true
+      Bullet.add_footer = true
     end
 
     if Rails.env.test?
-      Bullet.raise         = true # raise an error if n+1 query occurs
+      Bullet.raise = true # raise an error if n+1 query occurs
     end
   end
 end
