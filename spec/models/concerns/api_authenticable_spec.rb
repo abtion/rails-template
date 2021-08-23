c87e5e56d854257a6200368cfe41263a3a855a1f
Thor-Bjørn Böhme <tb@abtion.com>
2021-08-23 08:40:03 +0200

    Fix: missing spec revealed by branch coverage
    Misunderstood the code. It avoids generating the same token twice.
    
    Associated task:
    https://app.asana.com/0/1142794766483633/1200808372258902/f

--- spec/models/concerns/api_authenticable_spec.rb
+++ spec/models/concerns/api_authenticable_spec.rb
@@ -4,32 +4,33 @@ require "rails_helper"
 
 RSpec.describe ApiAuthenticable, type: :model do
   describe "before_create" do
-    context "when class can be found by token" do
+    context "when authentication_token doesn't already exist" do
       it "sets an authentication_token automatically" do
         user = build(:user, authentication_token: nil)
+        allow(Devise).to receive(:friendly_token).and_call_original
+
         user.save
 
         expect(user.authentication_token).to_not be_nil
         expect(user.authentication_token.length).to eq(
           ApiAuthenticable::AUTHENTICATION_TOKEN_LENGTH
         )
+        expect(Devise).to have_received(:friendly_token).once
       end
     end
 
-    context "when class already has token" do
-      it "does not alter theauthentication_token" do
+    context "when authentication_token already taken by other class" do
+      it "generates a new token" do
         old_token = Devise.friendly_token(ApiAuthenticable::AUTHENTICATION_TOKEN_LENGTH)
         new_token = Devise.friendly_token(ApiAuthenticable::AUTHENTICATION_TOKEN_LENGTH)
-        user = build(:user, authentication_token: old_token)
 
-        allow(Devise).to receive(:friendly_token).and_return(old_token, new_token)
-        allow(User).to receive(:find_by).and_return(true, false)
+        allow(Devise).to receive(:friendly_token).and_return(old_token, old_token, new_token)
 
-        user.save
+        create(:user, authentication_token: old_token)
+        user = create(:user, authentication_token: old_token)
 
         expect(user.authentication_token).to eq(new_token)
-        expect(User).to have_received(:find_by).twice
-        expect(Devise).to have_received(:friendly_token).twice
+        expect(Devise).to have_received(:friendly_token).thrice
       end
     end
   end
