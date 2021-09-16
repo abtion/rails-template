d5258eb567d5288d722c843a50a34661c1bf3c2e
Jesper Sørensen <jeppester1@gmail.com>
2021-09-16 13:35:34 +0200

    Tailwind: Set correct sans-serif font
    Co-authored-by: MarkusAbtion <MarkusAbtion@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
   },
   variants: {},
   plugins: [],
