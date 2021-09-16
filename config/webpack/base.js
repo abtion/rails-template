a415ab90d985b9a43e4b57c84848dd128ab7e7a6
Jesper Sørensen <jeppester1@gmail.com>
2021-09-16 12:16:58 +0200

    Remove bootstrap

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,12 +1,3 @@
 const { webpackConfig } = require("@rails/webpacker")
 
-const webpack = require("webpack")
-
-webpackConfig.plugins.unshift(
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery",
-  })
-)
-
 module.exports = webpackConfig
