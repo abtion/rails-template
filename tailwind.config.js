2a1f8f137818a7d689d5946bc124a927abdc2bfa
Jesper Sørensen <jeppester1@gmail.com>
2021-09-23 17:39:47 +0200

    Pick a dark or light contrast color for every color variant
    Unless if a contrast color is manually set

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 // tailwind.config.js
 
+console.log(require("./app/packs/const/colors").tailwindConfig)
+
 module.exports = {
   purge: ["./app/packs/**/*.{js,jsx,ts,tsx,scss,css}", "./app/views/**/*.erb"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: require("./app/packs/const/colors").tailwindConfig,
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      ...require("./app/packs/const/colors").tailwindConfig,
+    },
     fontFamily: {
       sans: ["Inter", "sans-serif"],
     },
