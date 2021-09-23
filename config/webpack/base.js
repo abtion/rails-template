828064eb3e550471cbc7cc0187ad0a37ba840482
Jesper Sørensen <jeppester1@gmail.com>
2021-09-23 12:58:45 +0200

    Restructure CSS to be more component oriented

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,3 +1,15 @@
 const { webpackConfig } = require("@rails/webpacker")
 
+const nodeSassGlobImporter = require("node-sass-glob-importer")
+
+const sassRule = webpackConfig.module.rules.find((rule) =>
+  [].concat(rule.test).some((test) => test.test(".scss"))
+)
+const sassLoader = sassRule.use.find((use) => {
+  const loader = typeof use === "object" ? use.loader : use
+  return loader.includes("sass-loader")
+})
+
+sassLoader.options.sassOptions.importer = nodeSassGlobImporter()
+
 module.exports = webpackConfig
