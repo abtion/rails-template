591d1c7a883352ef93ef796876d880c6b04a7a79
Jesper Sørensen <jeppester1@gmail.com>
2021-09-27 13:24:30 +0200

    Eslint: Lint typescript

--- app/packs/entrypoints/application.ts
+++ app/packs/entrypoints/application.ts
@@ -1,12 +1,15 @@
+import ujs from "@rails/ujs"
+import activestorage from "@rails/activestorage"
+
 import { cssVariables } from "../const/colors"
+import "../application.scss"
 
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
+ujs.start()
+activestorage.start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,8 +18,6 @@ require("@rails/activestorage").start()
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 require.context("../images", true)
-
-require("../application.scss")
 
 // Define CSS variables for all colors
 // These variables are used by tailwind.
