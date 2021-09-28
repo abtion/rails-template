882f5b15e8fe2cce2391828bca3ea0ed6a69ee6f
Jesper Sørensen <jeppester1@gmail.com>
2021-09-28 14:56:59 +0200

    Add react_component helper

---
+++ app/packs/util/mountComponents.test.tsx
@@ -0,0 +1,146 @@
+import React from "react"
+import mountComponents from "./mountComponents"
+
+interface ComponentsByPath {
+  [key: string]: Record<string, unknown>
+}
+
+const buildContextMock = (componentsByPath: ComponentsByPath) => {
+  const context = (path: string) => {
+    const component = componentsByPath[path]
+
+    if (!component) throw new Error(`Cannot find module '${path}'`)
+
+    return component
+  }
+
+  context.keys = () => Object.keys(componentsByPath)
+
+  return context
+}
+
+function TestComponent({ title }) {
+  return <h1 className="TestComponent">{title}</h1>
+}
+
+describe(mountComponents, () => {
+  afterEach(() => {
+    document.body.innerHTML = ""
+  })
+
+  it("mounts default exports", () => {
+    const element = document.createElement("div")
+    element.setAttribute("data-react-component", "TestComponent")
+    element.setAttribute("data-react-props", '{"title":"Title"}')
+    document.body.appendChild(element)
+
+    const context = buildContextMock({
+      "./TestComponent": { __esModule: true, default: TestComponent },
+    })
+
+    mountComponents(context)
+
+    expect(document.body.querySelector(".TestComponent").tagName).toBe("H1")
+    expect(document.body.querySelector(".TestComponent")).toHaveTextContent(
+      "Title"
+    )
+  })
+
+  it("mounts named exports", () => {
+    const element = document.createElement("div")
+    element.setAttribute("data-react-component", "List.TestComponent")
+    element.setAttribute("data-react-props", '{"title":"Title"}')
+    document.body.appendChild(element)
+
+    const context = buildContextMock({
+      "./List": { TestComponent },
+    })
+
+    mountComponents(context)
+
+    expect(document.body.querySelector(".TestComponent").tagName).toBe("H1")
+    expect(document.body.querySelector(".TestComponent")).toHaveTextContent(
+      "Title"
+    )
+  })
+
+  it("searches multiple contexts", () => {
+    const element = document.createElement("div")
+    element.setAttribute("data-react-component", "List.TestComponent")
+    element.setAttribute("data-react-props", '{"title":"Title"}')
+    document.body.appendChild(element)
+
+    const contextA = buildContextMock({})
+    const contextB = buildContextMock({
+      "./List": { TestComponent },
+    })
+
+    mountComponents(contextA, contextB)
+
+    expect(document.body.querySelector(".TestComponent").tagName).toBe("H1")
+  })
+
+  describe("when component name is invalid", () => {
+    /* eslint-disable no-console */
+    const originalConsoleLog = console.log
+    const originalConsoleError = console.error
+
+    beforeEach(() => {
+      console.log = jest.fn()
+      console.error = jest.fn()
+    })
+
+    afterEach(() => {
+      console.log = originalConsoleLog
+      console.error = originalConsoleError
+    })
+
+    it("prints useful error messages", () => {
+      const element = document.createElement("div")
+      element.setAttribute("data-react-component", "TestComponent")
+      element.setAttribute("data-react-props", '{"title":"Title"}')
+      document.body.appendChild(element)
+
+      const contextA = buildContextMock({
+        "./Component1": {},
+      })
+      const contextB = buildContextMock({
+        "./Component2": {},
+        "./Component3": {},
+      })
+
+      mountComponents(contextA, contextB)
+
+      expect(console.error).toHaveBeenCalledWith(
+        'Cannot find module "./TestComponent"'
+      )
+      expect(console.log).toHaveBeenCalledWith("Valid paths are:", [
+        "./Component1",
+        "./Component2",
+        "./Component3",
+      ])
+    })
+
+    /* eslint-enable no-console */
+  })
+
+  describe("when component fails to load", () => {
+    it("prints useful error messages", () => {
+      const element = document.createElement("div")
+      element.setAttribute("data-react-component", "TestComponent")
+      element.setAttribute("data-react-props", '{"title":"Title"}')
+      document.body.appendChild(element)
+
+      const contextMock = () => {
+        // Throw error when the context is called.
+        // This is what happens if a component fails to load
+        throw new Error("Uncaught ReferenceError: something is not defined")
+      }
+      contextMock.keys = () => []
+
+      expect(() => {
+        mountComponents(contextMock)
+      }).toThrow("Uncaught ReferenceError: something is not defined")
+    })
+  })
+})
---
+++ app/packs/util/mountComponents.ts
@@ -0,0 +1,69 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+interface RequireContext {
+  (id: string)
+  keys(): string[]
+}
+
+function getValidPaths(contexts: RequireContext[]): string[] {
+  return contexts.reduce((res, context) => res.concat(context.keys()), [])
+}
+
+function requireComponent(requirePath: string, contexts: RequireContext[]) {
+  for (const context of contexts) {
+    try {
+      // Loading a component will raise an error if it fails
+      // but we want to be able to try multiple contexts
+      return context(requirePath)
+    } catch (error) {
+      if (error.message.includes("Cannot find module")) continue
+      throw error
+    }
+  }
+}
+
+function getConstructor(className: string, contexts: RequireContext[]) {
+  const parts = className.split(".")
+  const filename = parts.shift()
+  const keys = parts
+  const requirePath = `./${filename}`
+
+  // Load the module:
+  let component = requireComponent(requirePath, contexts)
+
+  if (!component) {
+    console.error(`Cannot find module "${requirePath}"`) // eslint-disable-line
+    console.log("Valid paths are:", getValidPaths(contexts)) // eslint-disable-line
+    return
+  }
+
+  // Then access each key:
+  keys.forEach(function (k) {
+    component = component[k]
+  })
+
+  // support `export default`
+  if (component.__esModule) {
+    component = component["default"]
+  }
+  return component
+}
+
+export default function mountComponent(...contexts: RequireContext[]): void {
+  const nodes = document.querySelectorAll("[data-react-component]")
+
+  for (let i = 0; i < nodes.length; ++i) {
+    const node = nodes[i]
+    const className = node.getAttribute("data-react-component")
+    const constructor = getConstructor(className, contexts)
+
+    if (!constructor) continue
+
+    const propsJson = node.getAttribute("data-react-props")
+    const props = propsJson && JSON.parse(propsJson)
+
+    const component = React.createElement(constructor, props)
+    ReactDOM.render(component, node)
+  }
+}
