5ccfa2c432f8df46c82d999e0df32b40f22e08f6
Jesper Sørensen <jeppester1@gmail.com>
2021-10-04 10:45:16 +0200

    Setup jest to enforce 100% coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,19 @@ module.exports = {
   roots: ["app/packs"],
   testRegex: ".*test.(t|j)sx?$",
   testEnvironment: "jsdom",
+  collectCoverageFrom: [
+    "app/packs/**/*.{js,ts,tsx}",
+    "!app/packs/entrypoints/*",
+    "!app/packs/**/*.d.ts",
+  ],
+  coverageThreshold: {
+    "app/packs/**/*.{js,ts,tsx}": {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   setupFilesAfterEnv: ["./app/packs/jest.setup.ts"],
   moduleFileExtensions: ["js", "ts", "json", "tsx", "scss"],
   moduleNameMapper: {
