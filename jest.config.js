b287fa97d6fe94a8df92319a70a2dc6f0bc58336
Jesper Sørensen <jeppester1@gmail.com>
2021-10-13 14:05:32 +0200

    Jest: Add mock for static file imports

--- jest.config.js
+++ jest.config.js
@@ -19,5 +19,10 @@ module.exports = {
   moduleFileExtensions: ["js", "ts", "json", "tsx", "scss"],
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy",
+
+    // These are all the static file formats that webpacker comes preconfigured with support for:
+    // https://github.com/rails/webpacker/blob/e039a2f27e6bb684e921081295d3408625d25799/package/rules/file.js
+    "\\.(bmp|gif|jpeg|jpg|png|tiff|ico|avif|webp|eot|otf|ttf|woff|woff2|svg)":
+      "<rootDir>/app/packs/__mocks__/staticFileMock.js",
   },
 }
