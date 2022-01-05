6955f615d13b63591fce6b2fe203252d937bdb8c
Jesper Sørensen <jeppester1@gmail.com>
2022-01-05 08:26:10 +0100

    Content security policy: Re-add policy for webpack-dev-server

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -21,6 +21,11 @@
 #   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
 #   config.content_security_policy_nonce_directives = %w(script-src)
 #
+#   # If you are using webpack-dev-server then specify webpack-dev-server host
+#   if Rails.env.development?
+#     policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035"
+#   end
+#
 #   # Report CSP violations to a specified URI. See:
 #   # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 #   # config.content_security_policy_report_only = true
