ab7f941c970fa28d3f8d2bd5b0409c8d56c607b6
Reinis Laudansks <rl@abtion.com>
2022-05-17 11:10:43 +0200

    Add rails-controller-testing test example
    Co-authored-by: martinvintherp <mp@abtion.com>

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe HomeController, type: :controller do
     it "is ok" do
       get :index
 
+      expect(assigns(:test)).to eq("test")
       expect(response).to have_http_status(:ok)
     end
   end
