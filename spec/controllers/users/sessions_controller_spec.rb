44fdb6a84f1a22b360887548b184b2957574f27d
Reinis Laudansks <rl@abtion.com>
2022-05-23 10:37:02 +0200

    Respond with pwned password warning in after_sign_in_path_for if users password has been pwned
    Co-authored-by: Axel Rosén <ar@abtion.com>

---
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Users::SessionsController, type: :controller do
+  describe "#create" do
+    context "with a pwned password" do
+      it "allows sign in, but warns in a flash message" do
+        request.env["devise.mapping"] = Devise.mappings[:user]
+        user = create(:user, :admin, email: "some@domain.com", password: "woof-SAYS-muffi")
+        stub_weak_password(password: "woof-SAYS-muffi", breach_count: 3_000)
+
+        post :create, params: { user: { email: user.email, password: user.password } }
+
+        expect(response).to redirect_to(root_path)
+        expect(flash[:warning]).to eq(
+          "Please note that the password you are using has been displayed
+          in 3000 data breaches lately as a known used password -
+          we suggest that you change your current password.".squish
+        )
+      end
+    end
+  end
+end
