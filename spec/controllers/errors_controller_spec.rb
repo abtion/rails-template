b4354d4fffd8778f519928c1138b9f87525dedda
Viesturs (Abtion) <vb@abtion.com>
2022-05-31 09:19:24 +0200

    Handle routing errors (#979)
    * Redirect to custom :not_found page only on production ENV
    
    The default missing route page is helpful in dev
    
    Co-authored-by: Martin Petersen <mp@abtion.com>
    
    * Send RoutingError to rollbar only if referer is present
    
    Co-authored-by: Martin Petersen <mp@abtion.com>
    
    Co-authored-by: Martin Petersen <mp@abtion.com>

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -4,14 +4,34 @@ require "rails_helper"
 
 RSpec.describe ErrorsController, type: :controller do
   describe "GET #not_found" do
-    it "renders 404 status page" do
-      Rails.application.routes.draw { get "/not_found" => "errors#not_found" }
+    context "when request has no referer" do
+      it "renders 404 status page and does not send warning to Rollbar" do
+        allow(Rollbar).to receive(:warning)
+        Rails.application.routes.draw { get "/not_found" => "errors#not_found" }
 
-      get :not_found
+        get :not_found
 
-      expect(response).to have_http_status(:not_found)
+        expect(response).to have_http_status(:not_found)
+        expect(Rollbar).to_not(have_received(:warning))
 
-      Rails.application.reload_routes!
+        Rails.application.reload_routes!
+      end
+    end
+
+    context "when request has a referer" do
+      it "renders 404 status page and sends warning to Rollbar" do
+        allow(Rollbar).to receive(:warning).and_return("success")
+        Rails.application.routes.draw { get "/not_found" => "errors#not_found" }
+
+        controller.request.headers.merge({ HTTP_REFERER: "https://example.com/" })
+
+        get :not_found
+
+        expect(response).to have_http_status(:not_found)
+        expect(Rollbar).to have_received(:warning).with("RoutingError: No route matches [GET] 'http://test.host/not_found'")
+
+        Rails.application.reload_routes!
+      end
     end
   end
 end
