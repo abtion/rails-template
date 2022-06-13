ab74942cedad04df7c4e0e66c388a039274d06cf
Reinis Laudansks <rl@abtion.com>
2022-06-13 12:33:35 +0200

    Rename class field_with_errors -> Input--error

--- config/initializers/errors.rb
+++ config/initializers/errors.rb
@@ -3,7 +3,7 @@
 ActionView::Base.field_error_proc = proc do |html_tag, _instance|
   if /<(input|textarea|select)/.match?(html_tag)
     html_field = Nokogiri::HTML::DocumentFragment.parse(html_tag)
-    html_field.children.add_class("field_with_errors")
+    html_field.children.add_class("Input--error")
     ActiveSupport::SafeBuffer.new(html_field.to_s)
   else
     html_tag
