2176a53c3972c4333fb5ce458c54e44d5e99c41b
Jesper Sørensen <jeppester1@gmail.com>
2022-07-13 10:40:57 +0200

    Style admin user show view

--- spec/views/admin/users/show.html.erb_spec.rb
+++ spec/views/admin/users/show.html.erb_spec.rb
@@ -9,22 +9,22 @@ RSpec.describe "admin/users/show", type: :view do
 
     render
 
-    expect(rendered).to have_content("Admin Area")
-    expect(rendered).to have_content("User")
+    expect(rendered).to have_content(user.email)
 
+    expect(rendered).to have_link("Back", href: admin_users_path.to_s)
     expect(rendered).to have_link("Edit", href: edit_admin_user_path(user).to_s)
     expect(rendered).to have_css(
       "a[href='#{admin_user_path(user)}'][data-method='delete'][data-confirm='Are you sure?']",
       text: "Delete"
     )
 
-    expect(rendered).to have_css("tr td", text: "Email")
+    expect(rendered).to have_css("tr th", text: "Email")
     expect(rendered).to have_css("tr td", text: user.email.to_s)
 
-    expect(rendered).to have_css("tr td", text: "Name")
+    expect(rendered).to have_css("tr th", text: "Name")
     expect(rendered).to have_css("tr td", text: user.name.to_s)
 
-    expect(rendered).to have_css("tr td", text: "Created at")
+    expect(rendered).to have_css("tr th", text: "Created at")
     expect(rendered).to have_css("tr td", text: user.created_at.strftime("%d %b %H:%M").to_s)
   end
 end
