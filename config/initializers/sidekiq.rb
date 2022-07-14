14b7f8c7084eb1dfd306598e498d23c9e6d55c41
Jesper Sørensen <jeppester1@gmail.com>
2022-07-14 09:45:41 +0200

    Allow self signed certificates for redis
    Heroku redis uses a self-signed certificate, so we need this change for action cable and sidekiq to work on heroku.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -17,10 +17,15 @@ if Rails.env.production?
   end
 end
 
+redis_config = {
+  url: ENV.fetch("REDIS_URL"),
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+}
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") }
+  config.redis = redis_config
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") }
+  config.redis = redis_config
 end
