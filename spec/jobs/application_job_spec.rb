5e4fff4c96ef175eddceb23800854796ebfd70f1
Jesper Sørensen <jeppester1@gmail.com>
2023-03-21 14:12:46 +0100

    application job: Add example for spec

--- spec/jobs/application_job_spec.rb
+++ spec/jobs/application_job_spec.rb
@@ -3,5 +3,18 @@
 require "rails_helper"
 
 RSpec.describe ApplicationJob do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be used as basis for jobs" do
+    rocket_class = double("Rocket")
+    allow(rocket_class).to receive(:launch_all!)
+    stub_const("Rocket", rocket_class)
+
+    test_job = Class.new(ApplicationJob) do
+      def perform
+        Rocket.launch_all!(thrusters: true)
+      end
+    end
+
+    expect { test_job.perform_now }.to_not raise_error
+    expect(Rocket).to have_received(:launch_all!).with(thrusters: true)
+  end
 end
