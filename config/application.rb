87d908404548b9f7aa6c4b4c8a92f73ba121fe8a
Jesper Sørensen <jeppester1@gmail.com>
2023-05-15 07:27:34 +0200

    Set default time zone

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,8 @@ module ProjectNamePascal
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = "Europe/Copenhagen"
   end
 end
