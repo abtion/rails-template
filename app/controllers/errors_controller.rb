95787c7fef88ee242d4cd3e7141d5d99e4fdb920
David <10492210+davideluque@users.noreply.github.com>
2024-01-22 16:56:34 +0100

    Ignore predefined patterns when sending RoutingError to Rollbar
    https://app.asana.com/0/1142794766483633/1204570621030669

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -12,13 +12,21 @@ class ErrorsController < ActionController::Base
       format.any { render json: { error: "Page not found" }, status: :not_found }
     end
 
-    return unless request.referer
+    return if !request.referer || ignored_route_pattern?
 
     Rollbar.warning(
       ["RoutingError: No route matches",
        "[#{request.method}]",
        "'#{request.original_url}'"].join(" ")
     )
+  end
+
+  private
+
+  def ignored_route_pattern?
+    [".php", ".xml", ".txt", ".css", ".zip", ".gz"].any? do |ignored_route_pattern|
+      request.path.downcase.include?(ignored_route_pattern)
+    end
   end
 end
 # rubocop:enable Rails/ApplicationController
