2aa7693d619f14913e4e6314b140675070a6a05b
Jesper Sørensen <js@abtion.com>
2024-02-26 08:29:19 +0100

    Refactor example service (#1986)
    * Docker: Fix incorrect volume name for postgres
    
    * ExampleService: Use exception instead of the service result class

--- spec/services/example_service_spec.rb
+++ spec/services/example_service_spec.rb
@@ -4,18 +4,21 @@ require "rails_helper"
 
 RSpec.describe ExampleService, type: :service do
   describe "#execute" do
-    it "returns a ServiceResult" do
-      service = ExampleService.new("Hello, world!")
-      result = service.execute
-
-      expect(result).to be_a(ServiceResult)
-    end
-
     it "transforms the message to uppercase" do
       service = ExampleService.new("Hello, world!")
       result = service.execute
 
-      expect(result.payload).to eq("HELLO, WORLD!")
+      expect(result).to eq("HELLO, WORLD!")
+    end
+
+    context "when message is 'invalid'" do
+      it "raises with InvalidMessageError" do
+        service = ExampleService.new("invalid")
+
+        expect do
+          service.execute
+        end.to raise_error(ExampleService::InvalidMessageError)
+      end
     end
   end
 end
