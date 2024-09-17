ec322c3eedb4be1b53ae818ef475aa97f1b48863
Rune Philosof <rp@abtion.com>
2024-09-17 08:47:56 +0200

    Lint: Autocorrect `Rails/RootPathnameMethods`
    Fixes
    ```
    config/initializers/locale.rb:7:30: C: [Correctable] Rails/RootPathnameMethods: Rails.root is a Pathname, so you can use Rails.root.glob("config/locales/**/*.yml").
        config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.yml")]
                                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    ```

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -4,6 +4,6 @@ module ProjectNamePascal
   class Application < Rails::Application
     config.i18n.available_locales = [:da, :en]
     config.i18n.default_locale = ENV.fetch("LOCALE", "en")
-    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.yml")]
+    config.i18n.load_path += Rails.root.glob("config/locales/**/*.yml")
   end
 end
