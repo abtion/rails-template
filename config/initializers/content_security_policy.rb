87bab901f2b937239e6ab275ddcd492e3ffb71f9
Jesper Sørensen <js@abtion.com>
2024-10-10 10:54:28 +0200

    Reenable and refactor CSP (#2190)
    * Reenable and refactor CSP
    
    It was disabled by accident by `rails app:update` in 6ee7ee6.
    
    Refactor it to make it easier to group additions to the csp,
    preventing a mess of added hosts.
    
    Co-authored-by: Thies Pierdola <thiespierdola@gmail.com>
    
    * Fix CSP on first request before session is created
    
    As suggested in
    https://github.com/rails/rails/pull/48510#discussion_r1327968116
    
    Optimally, we would not have to make a session for unauthenticated
    users, but we are already always creating a session for CSRF (just a bit
    too late in the process).
    
    * Fix b7fe385: `next_session_id` is not a thing
    
    Also, not the comment that the commit was linking to.
    Accidentally committed old working tree.
    
    * Fix too long line
    
    ---------
    
    Co-authored-by: Thies Pierdola <thiespierdola@gmail.com>
    Co-authored-by: Rune Philosof <rp@abtion.com>

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,28 +6,50 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap and inline scripts
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src)
-#
-#   # If you are using webpack-dev-server then specify webpack-dev-server host
-#   if Rails.env.development?
-#     policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035"
-#   end
-#
-#   # Report CSP violations to a specified URI. See:
-#   # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-#   # config.content_security_policy_report_only = true
-# end
+CONTENT_SECURITY_POLICY_MAP = {
+  base_uri: [:none],
+  form_action: [:self],
+  frame_ancestors: [:none],
+  connect_src: [:self],
+  default_src: [:self],
+  font_src: [:self],
+  img_src: [:self],
+  object_src: [:none],
+  script_src: [:self],
+  style_src: [:self]
+}.freeze
+
+Rails.application.configure do
+  # Add Rollbar to the content security policy
+  CONTENT_SECURITY_POLICY_MAP[:connect_src] << "https://api.rollbar.com"
+  CONTENT_SECURITY_POLICY_MAP[:script_src] << "https://cdn.rollbar.com"
+
+  # If you are using shakapacker-dev-server then specify shakapacker-dev-server host
+  if Rails.env.development?
+    CONTENT_SECURITY_POLICY_MAP[:connect_src].push("http://localhost:3035", "ws://localhost:3035")
+  end
+
+  config.content_security_policy do |policy|
+    policy.base_uri(*CONTENT_SECURITY_POLICY_MAP[:base_uri])
+    policy.form_action(*CONTENT_SECURITY_POLICY_MAP[:form_action])
+    policy.frame_ancestors(*CONTENT_SECURITY_POLICY_MAP[:frame_ancestors])
+    policy.default_src(*CONTENT_SECURITY_POLICY_MAP[:default_src])
+    policy.font_src(*CONTENT_SECURITY_POLICY_MAP[:font_src])
+    policy.img_src(*CONTENT_SECURITY_POLICY_MAP[:img_src])
+    policy.object_src(*CONTENT_SECURITY_POLICY_MAP[:object_src])
+    policy.script_src(*CONTENT_SECURITY_POLICY_MAP[:script_src])
+    policy.style_src(*CONTENT_SECURITY_POLICY_MAP[:style_src])
+    policy.connect_src(*CONTENT_SECURITY_POLICY_MAP[:connect_src])
+    # Specify URI for violation reports
+    # policy.report_uri "/csp-violation-report-endpoint"
+  end
+
+  # Generate session nonces for permitted importmap, inline scripts, and inline styles.
+  config.content_security_policy_nonce_generator = lambda { |request|
+    request.session[:nonce] ||= SecureRandom.hex
+  }
+  config.content_security_policy_nonce_directives = %w[script-src style-src]
+
+  # Report violations without enforcing the policy.
+  # config.content_security_policy_report_only = true
+end
