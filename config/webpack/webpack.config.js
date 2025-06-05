d3a05617a2fa01d04ec7f70632fdc463bb8084ff
Jesper Sørensen <jeppester1@gmail.com>
2025-06-05 14:02:41 +0000

    Get rid of CSS warnings from webpack compilation

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -2,20 +2,10 @@ const { generateWebpackConfig, merge } = require("shakapacker")
 const webpackConfig = generateWebpackConfig()
 
 const aliasConfig = require("./alias")
+const PrependCssPlugin = require("./PrependCssPlugin")
 const colors = require("../../colors.json")
 const { getColorStyles } = require("@abtion-oss/design-system-colors")
-const nodeSassGlobImporter = require("node-sass-glob-importer")
 
-const sassRule = webpackConfig.module.rules.find((rule) =>
-  [].concat(rule.test).some((test) => test.test(".scss")),
-)
-const sassLoader = sassRule.use.find((use) => {
-  const loader = typeof use === "object" ? use.loader : use
-  return loader.includes("sass-loader")
+module.exports = merge({}, webpackConfig, aliasConfig, {
+  plugins: [new PrependCssPlugin(getColorStyles(colors))],
 })
-
-sassLoader.options.sassOptions.importer = nodeSassGlobImporter()
-sassLoader.options.api = "legacy"
-sassLoader.options.additionalData = getColorStyles(colors)
-
-module.exports = merge({}, webpackConfig, aliasConfig)
